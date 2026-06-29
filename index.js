1a0c7b2145ede0c5f5b1258577c27683f432f7f0
nida <nidhfz85@gmail.com>
2026-06-29 11:41:08 +0500

    upd

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let express=require("express");
 var mongoose=require("mongoose")
+let enquiryModel=require("./models/enquiry.model")
 require("dotenv").config()
 let app=express();
+app.use(express.json())
+
+app.post("/api/enquiry-insert",(req,res)=>{
+    let {name,email,phone,message} = req.body
+    let enquiry= new enquiryModel({
+        name:name,
+        email:email,
+        phone:phone,
+        message:message
+    })
+    res.send("data received")
+
+})
+
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log("Connected to mongoose")
     app.listen(process.env.PORT)
--- models/enquiry.model.js
+++ models/enquiry.model.js
@@ -8,5 +8,15 @@ let userEnquirySchema=mongoose.Schema({
         type:String,
         required:true,
         unique:true
-    }
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
 })
+let enquiryModel=mongoose.model("enquiry",userEnquirySchema)
+module.exports=enquiryModel
